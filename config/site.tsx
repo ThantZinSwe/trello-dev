4443eb7828376b171a6200603a9037f65dc59df5
thantzinswe <thantzinswe01@gmail.com>
2023-12-02 20:29:43 +0630

    Develop Clerk Authentication

---
+++ config/site.tsx
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: "Trello Dev",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+};
